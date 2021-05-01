48d4c182e405befdd55132544d5a39aa6552dd4b
Kevin Alberca <desarrollador.ext3@pymhse.com>
2021-05-01 01:15:34 -0500

    .env

--- src/Config/axios.js
+++ src/Config/axios.js
@@ -4,6 +4,7 @@ import axios from 'axios';
 export default axios.create({
     baseURL: process.env.VUE_APP_API+'api',
 
+    
     headers: {
         'Content-Type': 'application/json',
         //'Authorization':"Bearer " + localStorage.getItem('token')
